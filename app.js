54773899b6fa38f49ea52507116e1d29c63c0860
Md-Salehan <mdsalehan.official@gmail.com>
2022-05-15 00:44:13 +0530

    update password partially done

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const bodyParser = require("body-parser")
-const ejs = require("ejs")
+const ejs = require("ejs");
+const { use } = require("express/lib/application");
                                                                    
 const app = express()
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -8,12 +9,19 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    res.render("index")
+    fname = "";
+    res.render("index",{user: fname})
 })
 
 app.get('/:name', (req, res) => {
-    let name = req.params.name
+    let name = req.params.name;
     res.render("productPage",{imgName: name})
+})
+
+app.get('/user/newuser/:fname', (req, res) => {
+    let fname = req.params.fname;
+    if(fname === null) fname = "";
+    res.render("index",{user: fname})
 })
 
 app.get("/detail/:productName", (req, res) => {
@@ -26,6 +34,31 @@ app.get("/user/register", (req, res) => {
 })
 app.get("/user/login", (req, res) => {
     res.render("login");
+})
+app.get("/user/update-password", (req, res) => {
+    res.render("updatePassword")
+})
+app.post("/register", (req, res)=>{
+    let fname = req.body.fname;
+    let lname = req.body.lname;
+    let phoneNum = req.body.phoneNum;
+    let email = req.body.email;
+    let password = req.body.password;
+    let address1 = req.body.address1;
+    let address2 = req.body.address2;
+    let state = req.body.state;
+    let city = req.body.city;
+    let district = req.body.district;
+    let pinCode = req.body.pinCode;
+
+    res.redirect("/user/newuser/"+fname);
+})
+app.post("/update-password", (req, res)=>{
+    let oldPassword = req.body.oldPassword;
+    let newPassword = req.body.newPassword;
+    let confirmPassword = req.body.confirmPassword;
+
+    res.redirect("/");
 })
 app.listen(process.env.PORT || 3000, () => {
   console.log("server started");
